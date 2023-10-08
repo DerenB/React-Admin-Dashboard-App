fa7a11e43d644c226f9960dc7a3299cffc92985a
Deren B <47374311+DerenB@users.noreply.github.com>
2023-10-07 21:10:23 -0400

    Component Files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,7 +68,7 @@ const App = () => {
               <Route path="/financial" element="Financial" />
               <Route path="/color-mapping" element="ColorMapping" />
               <Route path="/pyramid" element="Pyramid" />
-              <Route path="/stack" element="Stack" />
+              <Route path="/stacked" element="Stacked" />
             </Routes>
           </div>
 
